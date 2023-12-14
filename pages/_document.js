81cf97e15b9256c7f79d775baabf82fc96f854e0
im-still-thinking <royaritra1990rocks@gmail.com>
2023-12-14 18:26:00 +0530

    meta fix

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Taco Tribe Shop</title>
+        <meta name="description" content="Taco taco moreee tacooo"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
